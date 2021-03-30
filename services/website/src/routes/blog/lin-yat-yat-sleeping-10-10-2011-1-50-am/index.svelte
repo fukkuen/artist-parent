9fadf8b4f810212a3578e20207c8a56465d10284
cliff <cliff.wong@ehlacademy.org>
2021-03-30 14:47:55 +0800

    add remaining posts

---
+++ services/website/src/routes/blog/lin-yat-yat-sleeping-10-10-2011-1-50-am/index.svelte
@@ -0,0 +1,18 @@
+<script context="module">
+  import postFetcher from "../_post-fetcher";
+
+  export function preload({ path }) {
+    return postFetcher(this.fetch, path.split("/").pop());
+  }
+</script>
+
+<script>
+  import BlogPostWrapper from "../../../components/blog-post/wrapper.svelte";
+  import BlogPost, { metadata } from "../../../blog-posts/2021-03-30-lin-yat-yat-sleeping-10-10-2011-1-50-am/index.svx";
+
+  export let post;
+</script>
+
+<BlogPostWrapper {post}>
+  <BlogPost />
+</BlogPostWrapper>
