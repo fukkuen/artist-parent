1f04d1ac7508a7b4f514f980a91dc4e6dd43693e
cliff <cliff.wong@ehlacademy.org>
2021-03-31 17:47:55 +0800

    bookmar page

---
+++ services/website/src/components/blog-post/previews/list-view.svelte
@@ -0,0 +1,24 @@
+<script>
+	import {locale} from 'svelte-i18n'
+	export let post;
+
+	const coverImageBasePath = `blog-posts/${
+			post.metadata.createdAt.split("T")[0]
+	}-${post.metadata.slug}/cover.jpg`;
+</script>
+
+<a href="blog/{post.metadata.slug}" class="grid grid-cols-3">
+	<div class="col-span-1 relative overflow-hidden bg-cover" style="background-image: url({coverImageBasePath})">
+		<div style="padding-top: 100%"></div>
+	</div>
+
+	<div class="bg-orange-400 col-span-2 py-4 px-2">
+		<h3 class="text-white text-p2 leading-tight">{post.metadata[`intro_title_${$locale}`]}</h3>
+		<div class="flex items-center">
+			<p class="text-sm flex-1 font-bold text-orange-500">{post.metadata[`artist_name_${$locale}`]}</p>
+			<div class="ml-4 flex-shrink-0 text-white">
+				<svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 36 36" class="w-4 fill-current"><path d="M31.48 19.244l-8.38 8.383 1.638 1.697L36 18.064 24.61 6.675l-1.64 1.697 8.512 8.51L0 16.886v2.36"></path></svg>
+			</div>
+		</div>
+	</div>
+</a>
--- services/website/src/routes/bookmark/index.svelte
+++ services/website/src/routes/bookmark/index.svelte
@@ -8,30 +8,32 @@
 <script>
 	export let posts
 	import {bookmarkStore} from "../../stores";
-	import Preview from '../../components/blog-post/previews/index.svelte'
+	import Preview from '../../components/blog-post/previews/list-view.svelte'
 	import {t} from 'svelte-i18n'
 
-	let saved = $bookmarkStore
-	console.log(saved)
-	let saved_posts = posts.filter(p => {
-		return saved.includes(p.metadata.record_no)
+	$: saved_posts = posts.filter(p => {
+		return $bookmarkStore.includes(p.metadata.record_no)
 	})
 </script>
 
-<div class="bg-orange-500 text-white">
-	<div class="max-w-screen-lg mx-auto px-8 py-16">
-		<h1 class="text-p3 font-bold">{$t('bookmark_page_title')}</h1>
-	</div>
-</div>
-
-<div class="max-w-screen-lg mx-auto mx-2">
-	{#if saved_posts.length}
-		{#each saved_posts as post}
-			<Preview {post}/>
-		{/each}
-	{:else}
-		<div class="text-center py-16">
-			{$t('bookmark_page_no_record')}
+<div class="pb-12">
+	<div class="bg-orange-500 text-white mb-1">
+		<div class="max-w-screen-lg mx-auto px-8 py-16">
+			<h1 class="text-p3 font-bold">{$t('bookmark_page_title')}</h1>
 		</div>
-	{/if}
+	</div>
+
+	<div class="max-w-screen-lg mx-auto mx-2">
+		{#if saved_posts.length}
+			<div class="grid gap-1">
+				{#each saved_posts as post}
+					<Preview {post}/>
+				{/each}
+			</div>
+		{:else}
+			<div class="text-center py-16">
+				{$t('bookmark_page_no_record')}
+			</div>
+		{/if}
+	</div>
 </div>
