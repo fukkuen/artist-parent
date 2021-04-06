da5566cc4f3d8ed550eb0270616f1f13e3ddd427
cliff <cliff.wong@ehlacademy.org>
2021-04-06 10:53:25 +0800

    fb share 3

--- services/website/src/components/blog-post/_share_button.svelte
+++ services/website/src/components/blog-post/_share_button.svelte
@@ -36,20 +36,8 @@
 		<Icon name="share"/>
 		<span class="ml-2">{$t('share')}</span>
 	{:else}
-		<div id="fb-root"></div>
-		<script>(function(d, s, id) {
-			var js, fjs = d.getElementsByTagName(s)[0];
-			if (d.getElementById(id)) return;
-			js = d.createElement(s); js.id = id;
-			js.src = "https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.0";
-			fjs.parentNode.insertBefore(js, fjs);
-		}(document, 'script', 'facebook-jssdk'));</script>
-
-		<!-- Your share button code -->
-<!--		https:// + $page.host + $page.path-->
-		<div class="fb-share-button"
-		     data-href="https://artist-parent.vercel.app/blog/instructionology"
-		     data-layout="button_count">
-		</div>
+		<a href="https://www.facebook.com/sharer/sharer.php?u=example.org" target="_blank">
+			Share on Facebook
+		</a>
 	{/if}
 </button>
