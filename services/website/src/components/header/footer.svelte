fb0fcad1733638bb75959f635eb360995cebdc8d
cliff <cliff.wong@ehlacademy.org>
2021-04-30 11:54:10 +0800

    footer

--- services/website/src/components/header/footer.svelte
+++ services/website/src/components/header/footer.svelte
@@ -2,40 +2,56 @@
 	import Icon from '../ui-elements/icon.svelte'
 	import {t, locale} from 'svelte-i18n'
 	import {categories} from "../../taxonomy";
-	import {languages_store} from "../../stores";
+	import {languages_store, preface_store} from "../../stores";
+
+	const about_menu = [
+		{
+			label: 'about_rooftop_title',
+			href: '/about-rooftop'
+		},
+		{
+			label: 'how_to_use_title',
+			href: '/how-to-use'
+		}
+	]
 </script>
 
 <div class="bg-gray-100 text-gray-500 border-t border-gray-300">
 	<div class="max-w-screen-lg mx-auto px-4 py-2 md:px-8 md:py-4 text-xs">
 		<div class="border-b border-gray-300 py-4">
 			<h3 class="font-bold mb-2">{$t('about')}</h3>
-			<div class="grid grid-cols-1 md:grid-cols-3 gap-3">
-				<div>sdfsd</div>
-				<div>sdfsd</div>
-				<div>sdfsd</div>
+			<div class="grid grid-cols-1 md:grid-cols-2 gap-3">
+				{#each about_menu as m}
+					<a href={m.href}>{$t(m.label)}</a>
+				{/each}
 			</div>
 		</div>
 		<div class="border-b border-gray-300 py-4">
 			<h3 class="font-bold mb-2">{$t('work_category')}</h3>
-			<div class="grid grid-cols-1 md:grid-cols-3 gap-3">
+			<div class="grid grid-cols-1 md:grid-cols-2 gap-3">
 				{#each categories as c}
 					<a href="/blog/category/{c.slug}/1">{c.num}. {c[`name_${$locale}`]}</a>
 				{/each}
 			</div>
 		</div>
-		<div class="border-b border-gray-300 py-4 flex justify-between items-center">
-			<div class="p-2 inline-flex items-center bg-white rounded-full border border-gray-400 shadow-inner overflow-hidden">
-				<Icon name="facebook" className="w-4"/>
-				<Icon name="ig" className="w-4 ml-2"/>
-				<div class="px-2 font-bold">
-					@artist.parents
+		<div class="py-4">
+			<div class="flex justify-center items-center">
+				<div class="p-2 inline-flex items-center bg-white rounded-full border border-gray-400 shadow-inner overflow-hidden">
+					<Icon name="facebook" className="w-4"/>
+					<Icon name="ig" className="w-4 ml-2"/>
+					<div class="px-2 font-bold">
+						@artist.parents
+					</div>
+				</div>
+				<div class="inline-flex ml-4">
+					{#each $languages_store as l}
+						<button on:click={() => {languages_store.setLanguage(l.key)}} class:bg-gray-300={l.key === $locale}
+						        class="mx-2 font-bold text-xs flex-shrink-0 w-6 h-6 rounded-full flex items-center justify-center">{l.label}</button>
+					{/each}
 				</div>
 			</div>
-			<div class="inline-flex">
-				{#each $languages_store as l}
-					<button on:click={() => {languages_store.setLanguage(l.key)}} class:bg-gray-300={l.key === $locale}
-					        class="text-xs flex-shrink-0 w-10 h-10 rounded-full flex items-center justify-center">{l.label}</button>
-				{/each}
+			<div class="text-center mt-4">
+				© {new Date().getFullYear()} Rooftop.
 			</div>
 		</div>
 	</div>
